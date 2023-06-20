64b42df110f6b735bc2c65e3d3e334d7198b279a
alejandro-alzate <alejandroalzatesanchez@gmail.com>
2023-06-20 17:06:59 -0500

    Adding `setNewEmptyWeights()`

--- main.lua
+++ main.lua
@@ -1,16 +1,52 @@
+love.graphics.setDefaultFilter('nearest', 'nearest')
 local mw = require("moon-weights")
+local lume = require("lume")
+local lurker = require("lurker")
+local lovebird = require("lovebird")
 local layers = {}
-
+local number1 = love.image.newImageData("1.png")
+local drawablenum1 = love.graphics.newImage(number1)
 --Pass a table telling the library the structure
 --First is always taken like the input layer
 --Last is always taken like the output layer
-layers[1] = 748
+layers[1] = 784
 layers[2] = 16
 layers[3] = 16
 layers[4] = 10
 
 --A black box for making a weights & biases structure
 --this will haunt me in the future
-local weights = mw.newWeigths(layers)
+--local NeuralWeights = mw.newWeights(layers)
 
-local NeuralNetwork mw.newNetwork(layers, weights)
+NeuralNetwork = mw.newNetwork(layers)
+NeuralNetwork:setNewEmptyWeights()
+
+	local index = 1
+	for i=1, number1:getHeight() do
+		for i2=1, number1:getWidth() do
+			NeuralNetwork.network[1][index] = number1:getPixel(i2-1, i-1)
+			index = index + 1
+		end
+	end
+
+love.graphics.setBackgroundColor(0.5,0.5,0.5)
+
+function love.update(dt)
+	lurker.update(dt)
+	lovebird.update(dt)
+end
+
+function love.draw()
+	for i=1, #NeuralNetwork.network[1] do
+		local x, y = i % number1:getHeight(), math.ceil(i / number1:getWidth())
+		if y == 0 then
+			y = number1:getHeight()
+		end
+		local value = tonumber(NeuralNetwork.network[1][i])
+		love.graphics.setColor(1 - value, value, 0)
+		love.graphics.rectangle("fill", x*10+30, y*10+30, 8, 8)
+	end
+	love.graphics.setColor(1,1,1,1)
+	love.graphics.draw(drawablenum1, 330, 40, 0, 10, 10)
+	love.graphics.print(love.timer.getFPS().. '\n')
+end
--- moon-weights.lua
+++ moon-weights.lua
@@ -1,3 +1,54 @@
---SO and ai a hot topic in this day and age
+--SO and AI a hot topic in this day and age
 --This is a proof of concept that will be awfully slow
 --But screw python!
+
+local moonWeights = {}
+moonWeights.__index = moonWeights
+
+function moonWeights.newWeights(structure)
+	assert(type(structure) == 'table', "New neural network weights $structure not accepted: \""..type(structure).."\"")
+	local newWeights = {}
+	for i=1, #structure do
+		newWeights[i] = {}
+	end
+	for i,v in ipairs(structure) do
+		print(i,v)
+		assert(type(v) == 'number', "New neural network $structure layer not accepted")
+		assert(v >= 1, "New neural network $structure layer cannot be less than 1")
+		for i2 = 1, v do
+			table.insert(newWeights[i], 0)
+		end
+	end
+end
+
+function moonWeights.newNetwork(structure, weights)
+	assert(type(structure) == 'table', "New neural network $structure not accepted: \""..type(structure).."\"")
+	local newNeuralNetwork = setmetatable({}, moonWeights)
+	newNeuralNetwork.network = {}
+
+	for i=1, #structure do
+		newNeuralNetwork.network[i] = {}
+	end
+	for i,v in ipairs(structure) do
+		assert(type(v) == 'number', "New neural network $structure layer not accepted")
+		assert(v >= 1, "New neural network $structure layer cannot be less than 1")
+		for i2 = 1, v do
+			table.insert(newNeuralNetwork.network[i], 0)
+		end
+	end
+
+	return newNeuralNetwork
+end
+
+--Sets a table with all the weight who the user can poke around
+function moonWeights:setNewEmptyWeights()
+	self.weights = {}
+	for i,v in ipairs(self.network) do
+		self.weights[i] = {}
+		for t,k in ipairs(self.network[i]) do
+			self.weights[i][t] = {weight = 0, bias = 0}
+		end
+	end
+end
+
+return moonWeights
